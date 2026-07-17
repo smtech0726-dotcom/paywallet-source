aff6bc7192d6e7f4150f623bb992c0f668100091
smtech0726-dotcom <smtech0726@gmail.com>
2026-07-17 22:50:37 +0530

    Update login.tsx

--- artifacts/paytm-clone/src/pages/login.tsx
+++ artifacts/paytm-clone/src/pages/login.tsx
@@ -51,7 +51,6 @@ export default function Login() {
               Terms & Privacy Policy
               </div>
               </div>
-
-            </div>
- );
-}
+           </div>
+         );
+       }
