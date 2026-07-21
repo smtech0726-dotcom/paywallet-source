b36c042535473a33aabe0e0dd9514752981731e7
smtech0726-dotcom <smtech0726@gmail.com>
2026-07-21 13:33:12 +0530

    Update button.tsx

--- artifacts/paytm-clone/src/components/ui/button.tsx
+++ artifacts/paytm-clone/src/components/ui/button.tsx
@@ -52,11 +52,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(
-  buttonVariants({ variant, size }),
-  className
-)
-        }
+       return (
+ <button
+   className={cn(buttonVariants({ variant, size }), className)}
+ >
         ref={ref}
         {...props}
       />
